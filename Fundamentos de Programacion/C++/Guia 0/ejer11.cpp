b6edadca492423928ec8bd118b402ef336276192
bahLtA <86940628+bahLtA@users.noreply.github.com>
2022-05-29 18:45:46 -0300

    Add files via upload

---
+++ Fundamentos de Programacion/C++/Guia 0/ejer11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string nombre_y_apellido;
+	int cantidad,precio_unitario;
+	float precio;
+	
+	cout<<"Ingrese el nombre y el apellido del cliente: ";
+	getline(cin,nombre_y_apellido);
+	cout<<"Ingrese la cantidad comprada del articulo: ";
+	cin>>cantidad;
+	cout<<"Ingrese el precio unitario: ";
+	cin>>precio_unitario;
+	precio=cantidad*precio_unitario;
+	cout<<"Monto total: $"<<precio<<endl;
+	precio-=precio*0.05;
+	cout<<"Monto luego del descuento: $"<<precio<<endl;
+	precio-=precio*0.21;
+	cout<<"Monto final: $"<<precio<<endl;
+	
+	return 0;
+}
+
---
+++ Fundamentos de Programacion/C++/Guia 0/ejer12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int capital_solicitado,tasa_anual,plazo;
+	float interes_simple,valor_cuota;
+	cout<<"Ingrese el capital solicitado"<<endl;
+	cin>>capital_solicitado;
+	cout<<"Ingrese la tasa anual"<<endl;
+	cin>>tasa_anual;
+	cout<<"Ingrese el plazo a pagar"<<endl;
+	cin>>plazo;
+	interes_simple=(capital_solicitado*tasa_anual*plazo)/(100*12);
+	valor_cuota=(capital_solicitado+interes_simple)/plazo;
+	cout<<"El valor de la cuota es de: $"<<valor_cuota;
+	return 0;
+}
+
