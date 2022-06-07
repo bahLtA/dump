a020088688b810e37fe85a1212dd2d9327e4b340
bahLtA <86940628+bahLtA@users.noreply.github.com>
2022-06-07 00:44:45 -0300

    Add files via upload

---
+++ Fundamentos de Programacion/C++/Guia 3/ejer28.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int cod,cant_pol,suma_polizas=0,cont,capital_asegurado,cuota_mensual,suma_capital=0;
+	float porc_menos10,porc_menos50,porc_mas50,porcentaje;
+	int suma_cuotas_mensuales,suma_comision=0,comision;
+	int codigo_maspol,mas_pol=0,codigo_mascom,mas_com=0;
+	
+	cout<<"Ingrese el porcentaje de menos de 10 polizas vendidas"<<endl;
+	cin>>porc_menos10;
+	porc_menos10/=100;
+	cout<<"Ingrese el porcentaje de menos de 50 polizas vendidas"<<endl;
+	cin>>porc_menos50;
+	porc_menos50/=100;
+	cout<<"Ingrese el porcentaje de 50 o mas polizas vendidas"<<endl;
+	cin>>porc_mas50;
+	porc_mas50/=100;
+	
+	cout<<"Ingrese el codigo de vendedor"<<endl;
+	cin>>cod;
+	while (cod!=0){
+		cout<<"Ingrese la cantidad de polizas vendidas"<<endl;
+		cin>>cant_pol;
+		suma_polizas+=cant_pol;
+		
+		if (cant_pol>mas_pol){
+			mas_pol=cant_pol;
+			codigo_maspol=cod;
+		}
+		
+		if (cant_pol<10){
+			porcentaje=porc_menos10;
+		}else if (cant_pol<50){
+			porcentaje=porc_menos50;
+		}else if (cant_pol>=50){
+			porcentaje=porc_mas50;
+		}
+		
+		cont=0;
+		suma_cuotas_mensuales=0;
+		
+		while (cont!=cant_pol){
+			cont++;
+			
+			cout<<"Ingrese el capital asegurado de la poliza"<<endl;
+			cin>>capital_asegurado;
+			suma_capital+=capital_asegurado;
+			cout<<"Ingrese la cuota mensual a pagar"<<endl;
+			cin>>cuota_mensual;
+			suma_cuotas_mensuales+=cuota_mensual;
+		}
+		comision=suma_cuotas_mensuales/porcentaje;
+		suma_comision+=comision;
+		
+		if (comision>mas_com){
+			mas_com=comision;
+			codigo_mascom=cod;
+		}
+		
+		cout<<"Ingrese el codigo de vendedor"<<endl;
+		cin>>cod;
+	}
+	cout<<"Total de polizas vendidas: "<<suma_polizas<<endl;
+	cout<<"Total de capital asegurado: $"<<suma_capital<<endl;
+	cout<<"Total a pagar en concepto de comision: $"<<suma_comision<<endl<<endl;
+	cout<<"Vendedor que mas polizas vendio: "<<codigo_maspol<<endl;
+	cout<<"Vendedor que recibio mas comision: "<<codigo_mascom<<endl;
+	
+	return 0;
+}
+
---
+++ Fundamentos de Programacion/C++/Guia 3/ejer30.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int cant_suc,cant_caj,cont=0,contaux,cont_falta=0;
+	int	cant_1000,cant_500,cant_200,cant_100;
+	int falt_1000,falt_500,falt_200,falt_100;
+	int sum_1000=0,sum_500=0,sum_200=0,sum_100=0;
+	string nombre;
+	bool falta;
+	
+	int max_1000 = 1000;
+	int max_500 = 2000;
+	int max_200 = 5000;
+	int max_100 = 5000;
+	
+	cout<<"Ingrese la cantidad de sucursales de banco"<<endl;
+	cin>>cant_suc;
+	
+	while (cont!=cant_suc){
+		cout<<"Ingrese el nombre de la sucursal"<<endl;
+		cin.ignore();
+		getline(cin,nombre);
+		cout<<"Ingrese la cantidad de cajeros automaticos en dicha sucursal"<<endl;
+		cin>>cant_caj;
+		
+		contaux=0;
+		falt_1000=0;
+		falt_500=0;
+		falt_200=0;
+		falt_100=0;
+		
+		while (contaux!=cant_caj){
+			falta=false;
+			cout<<"Ingrese la cantidad de billetes de $1000"<<endl;
+			cin>>cant_1000;
+			if (cant_1000<max_1000*0.1){
+				falta=true;
+				falt_1000+=max_1000-cant_1000;
+			}
+			cout<<"Ingrese la cantidad de billetes de $500"<<endl;
+			cin>>cant_500;
+			if (cant_500<max_500*0.1){
+				falta=true;
+				falt_500+=max_500-cant_500;
+			}
+			cout<<"Ingrese la cantidad de billetes de $200"<<endl;
+			cin>>cant_200;
+			if (cant_200<max_200*0.1){
+				falta=true;
+				falt_200+=max_200-cant_200;
+			}
+			cout<<"Ingrese la cantidad de billetes de $100"<<endl;
+			cin>>cant_100;
+			if (cant_100<max_100*0.1){
+				falta=true;
+				falt_100+=max_100-cant_100;
+			}
+			
+			if (falta){
+				cont_falta++;
+			}
+			
+			contaux++;
+		}
+		
+		cout<<"Sucursal: "<<nombre<<endl;
+		cout<<"Faltan "<<falt_1000<<" billetes de $1000"<<endl;
+		cout<<"Faltan "<<falt_500<<" billetes de $500"<<endl;
+		cout<<"Faltan "<<falt_200<<" billetes de $200"<<endl;
+		cout<<"Faltan "<<falt_100<<" billetes de $100"<<endl;
+		
+		sum_1000+=falt_1000;
+		sum_500+=falt_500;
+		sum_200+=falt_200;
+		sum_100+=falt_100;
+		
+		cont++;
+	}
+	
+	cout<<"Cajeros con faltante: "<<cont_falta<<endl;
+	cout<<"Hay que reponer: "<<endl;
+	cout<<sum_1000<<" billetes de $1000"<<endl;
+	cout<<sum_500<<" billetes de $500"<<endl;
+	cout<<sum_200<<" billetes de $200"<<endl;
+	cout<<sum_100<<" billetes de $100"<<endl;
+	
+	return 0;
+}
+
