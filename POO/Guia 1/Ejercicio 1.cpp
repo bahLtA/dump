036ba04a33020ac40e20bffd5c021e6b2164e2ba
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-15 16:55:06 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//a)Una funcion llamada division_entera() que devuelve el 
+//cociente entero y el resto de la division de 2 enteros
+void division_entera(int &cociente,int &resto,int a, int b);
+//b) Calcular el factorial de un numero int;
+int facto(int n);
+//c) Devuelve la hipotenusa de un triangulo rectangulo
+float hipot(float a,float b);
+//d) Intercambia los contenidos de 2 parametros tipo char, modificando ambos
+//parametros de llamada. No devuelve otro resultado
+void intercambio(char &a,char &b);
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
+
+void division_entera(int &cociente,int &resto, int a,int b){
+	cociente=a/b;
+	resto=a%b;
+}
+
+int facto(int n){
+	int res=n;
+	for(int i=n-1;i>1;i--){
+		res*=i;
+	}
+	return res;
+}
+
+float hipot(float a,float b){
+	float res;
+	
+	res=(a*a)+(b*b);
+	res=sqrt(res);
+	
+	return res;
+}
+void intercambio(char &a,char &b){
+	char aux;
+	
+	aux=a;
+	a=b;
+	b=aux;
+}
---
+++ POO/Guia 1/Ejercicio 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void Calcular(float &imp);
+
+int main(int argc, char *argv[]) {
+	
+	float imp;
+	
+	cout<<"Ingrese el importe bruto de la factura"<<endl;
+	cin>>imp;
+	
+	Calcular(imp);
+	
+	cout<<"El importe final es de: $"<<imp;
+	
+	return 0;
+}
+
+void Calcular(float &imp){
+	
+	if(imp>50){
+		imp=imp-(imp*0.05);
+	}
+	
+	imp=imp+(imp*0.13);
+}
+
