8147b935ee823df9ac276f94baac8ad33ccc5f93
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-20 15:35:21 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alumnos{
+	string nombre;
+	int edad;
+} alumno [457];
+
+void ValidarEdad(int &edad);
+
+int main(int argc, char *argv[]) {
+	
+	int edad;
+	
+	for(int i=0;i<457;i++){
+		cout<<"Ingrese el nombre del integrante "<<i+1<<endl;
+		getline(cin,alumno[i].nombre);
+		cout<<"Ingrese la edad del integrante "<<i+1<<endl;
+		cin>>edad;
+		
+		
+		ValidarEdad(edad);
+		
+		cin.ignore();
+		
+		alumno[i].edad=edad;
+		
+	}
+	
+	return 0;
+}
+
+void ValidarEdad(int &edad){
+	while(edad<12 || edad>90){
+		cout<<"Ingrese la edad nuevamente"<<endl;
+		cin>>edad;
+	}
+	
+}
---
+++ POO/Guia 1/Ejercicio 5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void FormaLinea(int n,char c='#');
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	char c;
+	
+	FormaLinea(7,'-');
+	cout<<endl;
+	FormaLinea(10);
+	
+	
+	return 0;
+}
+
+void FormaLinea(int n,char c){
+	for(int i=0;i<n;i++){
+		cout<<c;
+	}
+}
+
