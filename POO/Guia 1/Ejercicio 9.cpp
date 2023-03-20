587d687165c703b7c7177cd07c656dfbed0c5a1d
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-20 17:10:03 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 9.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n);
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	cin>>n;
+	
+	
+	for(int i=0;i<n;i++){
+		cout<<Fibonacci(i)<<"  ";
+	}
+	
+	return 0;
+}
+
+int Fibonacci(int n){
+	if(n<2){
+		return n;
+	}
+	return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
