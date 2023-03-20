cdc39c6e73f58f4802307ccf5d57d17a20807e43
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-20 16:48:45 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int Potencia(int n,int pot);
+
+int main(int argc, char *argv[]) {
+	
+	int n,pot;
+	
+	cin>>n;
+	cin>>pot;
+	
+	cout<<Potencia(n,pot);
+	
+	return 0;
+}
+
+int Potencia(int n,int pot){
+	if(pot==0){
+		return 1;
+	}else if(n>1){
+		return n*Potencia(n,pot-1);
+	}
+	return 1;
+}
