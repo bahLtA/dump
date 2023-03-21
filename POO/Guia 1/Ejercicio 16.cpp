cee72c00d7d6939c4da26933c29d01259507cdf5
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-21 20:31:48 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 16.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int anio;
+	
+	cout<<"Ingrese un año"<<endl;
+	cin>>anio;
+	
+	if( (anio%4==0 && anio%100!=0)||(anio%400==0)){
+		cout<<"El año es bisiesto"<<endl;
+	}else{
+		cout<<"El año no es bisiesto"<<endl;
+	}
+	return 0;
+}
+
