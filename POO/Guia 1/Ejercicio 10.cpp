8cb21c0887fd4f9ac9a006810a9cd36cb419a1fa
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-21 12:57:35 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void Descuento(float &tot,int cant);
+
+int main(int argc, char *argv[]) {
+	
+	float valor,total;
+	int cant;
+	
+	cout<<"Ingrese el valor del pantalon"<<endl;
+	cin>>valor;
+	cout<<"Ingrese la cantidad de pantalones comprados"<<endl;
+	cin>>cant;
+	
+	total=valor*cant;
+	Descuento(total,cant);
+	
+	cout<<"Total a pagar: $"<<total;
+	
+	return 0;
+}
+
+void Descuento(float &tot,int cant){
+	if(cant>=5){
+		tot=tot-(tot*0.25);
+	}else{
+		tot=tot-(tot*0.1);
+	}
+}
+
---
+++ POO/Guia 1/Ejercicio 11.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool EsPrimo(int n);
+
+int main(int argc, char *argv[]) {
+	
+	int n;
+	bool primo;
+	
+	cout<<"Ingrese un numero"<<endl;
+	cin>>n;
+	
+	primo = EsPrimo(n);
+	
+	if(primo){
+		cout<<"El numero es primo"<<endl;
+	}else{
+		cout<<"El numero no es primo"<<endl;
+	}
+	
+	return 0;
+}
+
+bool EsPrimo(int n){
+	if(n==4) return 0;
+	for(int i=2;i<n;i++){
+		if(i%n==0){
+			return 0;
+		}
+	}
+	return 1;
+	
+}
+
