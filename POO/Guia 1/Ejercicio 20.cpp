4bc7741ab7ff75110870e28de8646bd16e7eab4e
Balthazar Regner <86940628+bahLtA@users.noreply.github.com>
2023-03-21 20:39:50 -0300

    Add files via upload

---
+++ POO/Guia 1/Ejercicio 20.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void EsVocal(char c){
+	if(c!='a' || c!='e' || c!='i' || c!='o' || c!='u'){
+		cout<<"La letra es una vocal";
+	}else{
+		cout<<"La letra es una consonante";
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	char c;
+	
+	cout<<"Ingrese una letra"<<endl;
+	cin>>c;
+	
+	EsVocal(c);
+	
+	return 0;
+}
+
